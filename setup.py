dcac9db90feda550cc583ad4321a4eebd09c0c77
Martin Vejnár <avakar@ratatanek.cz>
2015-09-15 18:19:36 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='cson',
+    version='0.1.0',
+
+    description='A parser for Coffeescript Object Notation (CSON)',
+    author='Martin Vejnár',
+    author_email='avakar@ratatanek.cz',
+    url='https://github.com/avakar/pycson',
+    license='MIT',
+
+    packages=['cson'],
+    )
