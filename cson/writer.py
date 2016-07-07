0351c992b1ce986d570d93b77e496a6ec9a3ea3c
Martin Vejnár <avakar@ratatanek.cz>
2016-07-07 12:12:19 +0200

    Fixed key escaping.

--- cson/writer.py
+++ cson/writer.py
@@ -19,7 +19,7 @@ else:
             return o
         return None
 
-_id_re = re.compile(r'[$a-zA-Z_][$0-9a-zA-Z_]*')
+_id_re = re.compile(r'[$a-zA-Z_][$0-9a-zA-Z_]*\Z')
 
 class CSONEncoder:
     def __init__(self, skipkeys=False, ensure_ascii=True, check_circular=True, allow_nan=True, sort_keys=False,
